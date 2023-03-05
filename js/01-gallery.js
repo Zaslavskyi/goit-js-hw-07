848fbe8fcfc0841c2701f99fff0ceffab76a2bc7
Zaslavskyi <bbaly89@gmail.com>
2023-03-06 00:49:07 +0200

    222

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,3 +5,18 @@ console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryImage = createGalleryImage(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
+galleryContainer.addEventListener('click', onGalleryModalClick);
+
+function createGalleryImage(gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `<div class="gallery__item"> 
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" width="340">
+        </a>
+        </div>`;
+    })
+    .join('');
+}
+
