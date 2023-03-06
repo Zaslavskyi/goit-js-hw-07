ec38da51b0d7a56058b46a41c628466f88856bff
Zaslavskyi <bbaly89@gmail.com>
2023-03-07 01:03:47 +0200

    ///

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryImage = createGalleryImage(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
+
+function createGalleryImage(gallery) {
+    return gallery.map(({ preview, original, description}) => {
+        
+    })
+    }
