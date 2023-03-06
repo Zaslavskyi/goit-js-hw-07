27cc8e184d630afb5265e4af9622592e38635692
Zaslavskyi <bbaly89@gmail.com>
2023-03-07 01:14:50 +0200

    ???

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,6 +10,18 @@ galleryContainer.insertAdjacentHTML('beforeend', galleryImage);
 
 function createGalleryImage(gallery) {
     return gallery.map(({ preview, original, description}) => {
-        
-    })
+        createImage(block);
+    });
+    .join('');
+    };
+
+    function createImage(block) {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}"
+        alt="${description}" width="340">
+        </a>
+        </div>
+        `;
     }
