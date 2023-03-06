2eb92fa7d5055334dfe9258efdd1aa7b7dac2c5d
Zaslavskyi <bbaly89@gmail.com>
2023-03-06 02:04:20 +0200

    i think it's done//

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,7 +13,7 @@ function createGalleryImage(gallery) {
     return gallery.map(({preview, original, description}) => {
         return `<div class="gallery__item"> 
         <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" width="340">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" width="340"/>
         </a>
         </div>`;
     })
@@ -25,9 +25,31 @@ function onGalleryModalClick(e) {
 
     if (!e.target.classList.contains('gallery__image')) {
         return;
-    }
+    };
 
-    onLightBoxModal(e) {
-        const instance = basicLightBox.create('<img src="${e.target.dataset.source}" width="1280" height="auto"/>')
-    }
-}
+    onLightBoxModal(e);
+};
+
+    function onLightBoxModal(e) {
+        const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="1280" height="auto"/>`, 
+        {
+            onShow: instance => {
+                window.addEventListener('keydown', onEscKeyPress);
+            },
+            onClose: instance => {
+                window.removeEventListener('keydown', onEscKeyPress);
+            },
+        },
+        );
+
+        instance.show();
+
+        function onEscKeyPress(e) {
+            const ESC_KEY_CODE = 'Escape';
+            const isEscKey = e.code === ESC_KEY_CODE;
+
+            if (isEscKey) {
+                instance.close();
+            };
+        };
+    };
